8c0b0e5f56310a83a011c1a6c85cf65192054d0f
bryan poetz <bryan.poetz@gmail.com>
2024-12-07 15:51:32 -0500

    day 1 star

--- bp-aoc/app/Main.hs
+++ bp-aoc/app/Main.hs
@@ -1,15 +1,18 @@
 module Main (main) where
+import Data.List
 
 main :: IO ()
 main = do 
-        contents <- readFile "../day_1.txt" 
-        print . splitCols . words $ contents
+        contents <- readFile "../day_1_test.txt" 
+        print . calculateDistance . transform . words $ contents
 
-
-bifurcate (x, index) acc | even index = (x: (fst acc), (snd acc))
-                         | not $ even index = ((fst acc), x:(snd acc))
+bifurcate (x, index) acc | even index = ((read x::Integer) : (fst acc), (snd acc))
+                         | not $ even index = ((fst acc), (read x::Integer) :(snd acc))
         
    
 
-splitCols xs=
+transform xs=
     foldr bifurcate ([],[]) (zip xs [0..])
+    
+calculateDistance (xs, ys) =
+    foldr (+) 0 $ map (\(x,y) -> abs (x - y)) $ zip (sort xs) (sort ys)

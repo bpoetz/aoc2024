8c489ec096042c7102774cb72e82edaece91e09c
bryan poetz <bryan.poetz@gmail.com>
2024-12-08 14:58:15 -0500

    day 2 star 2 complete

--- bp-aoc/app/Main.hs
+++ bp-aoc/app/Main.hs
@@ -4,7 +4,7 @@ import Data.List
 main :: IO ()
 main = do 
         contents <- readFile "../day_2.txt" 
-        print . safetyReport . lines $ contents
+        print . part2SafetyReport . lines $ contents
 
 pairs xs = zip xs (tail xs)
 -- returns 1 if the row is successful
@@ -17,7 +17,24 @@ check xs
             differentDirection = (\x -> ((head diffs) > 0) /= (x>0))
             noDirection = (\x -> (x == 0))
             bigJump = (\x -> (abs x) > 3)
-     
+
+
+multicheck xs
+      | (check xs) == 1 = 1
+      | otherwise = bruteForce xs
+      
+allButPosition 0 xs = tail xs
+allButPosition idx xs | length xs == idx = take (idx) xs  
+                      | otherwise = take (idx) xs ++ drop (idx+1) xs
+
 
 safetyReport xs =  foldr (+) 0 $ (map (\x -> check (words x)) xs)
+
+part2SafetyReport xs = foldr (+) 0 $ (map (\x -> multicheck (words x)) xs)
    
+bruteForce xs 
+   | any (\x -> check x == 1) [allButPosition idx xs | idx<-[0..listSize] ] = 1
+   | otherwise = 0
+   where listSize = length xs-1
+
+

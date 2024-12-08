932e081ac5bbd4f1c20e1e09b6dda4904404f979
bryan poetz <bryan.poetz@gmail.com>
2024-12-08 09:56:15 -0500

    day 2 stars

--- bp-aoc/app/Main.hs
+++ bp-aoc/app/Main.hs
@@ -1,18 +1,32 @@
 module Main (main) where
 import Data.List
+import qualified Data.Map as M
 
 main :: IO ()
 main = do 
-        contents <- readFile "../day_1_test.txt" 
-        print . calculateDistance . transform . words $ contents
+        contents <- readFile "../day_1.txt" 
+        print . similarity . transform . words $ contents
 
 bifurcate (x, index) acc | even index = ((read x::Integer) : (fst acc), (snd acc))
                          | not $ even index = ((fst acc), (read x::Integer) :(snd acc))
         
    
 
-transform xs=
+transform xs =
     foldr bifurcate ([],[]) (zip xs [0..])
     
 calculateDistance (xs, ys) =
     foldr (+) 0 $ map (\(x,y) -> abs (x - y)) $ zip (sort xs) (sort ys)
+
+similarity (xs, ys) = 
+    let simMap = foldr addToSimilarityMap M.empty ys
+    in foldr (+) 0 $ map (lookupSimilarity simMap) xs
+
+
+addToSimilarityMap k = 
+  M.insertWith (+) k 1
+
+
+lookupSimilarity dict k  
+  | M.member k dict= k * dict M.! k
+  | M.notMember k dict = 0
